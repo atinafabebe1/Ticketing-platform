fcbe183490fa0d83fda19a361e97ad640ceee194
atinafabebe1 <atinafabebe1@gmail.com>
2023-07-20 12:57:50 +0300

    Mount userRouter in the express app

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
 // Load environment variables from .env file
-dotenv.config();
 
 // Middleware
 app.use(express.json());
 
 // Routes
-// Add your routes here
+const userRouter = require('./routes/user');
+const CookieParser = require('cookieparser');
+// mount
+app.use('/api/user', userRouter);
 
 module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const app = require('./app');
 const connectDB = require('./config/db');
+const errorHandler = require('./middlewares/error');
+
+app.use(errorHandler);
 
 // Connect to MongoDB
 connectDB().then(() => {
     // Start the server
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3500;
     app.listen(port, () => {
         console.log(`Server running on port ${port}`);
     });
